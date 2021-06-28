842374b3cb9e989dbd9d5d3686f3bf8cd839b15a
Alie Langston <alangsto@wellesley.edu>
2021-06-28 09:30:45 -0500

    feat: add post endpoint for VerifiedName
    MST-796. Add post endpoint to create new verified name.

--- edx_name_affirmation/views.py
+++ edx_name_affirmation/views.py
@@ -11,7 +11,8 @@ from rest_framework.views import APIView
 
 from django.contrib.auth import get_user_model
 
-from edx_name_affirmation.api import get_verified_name
+from edx_name_affirmation.api import create_verified_name, get_verified_name
+from edx_name_affirmation.exceptions import VerifiedNameMultipleAttemptIds
 from edx_name_affirmation.serializers import VerifiedNameSerializer
 
 
@@ -25,8 +26,28 @@ class AuthenticatedAPIView(APIView):
 
 class VerifiedNameView(AuthenticatedAPIView):
     """
-    Endpoint for getting a verified name and its data.
+    Endpoint for a VerifiedName.
     /edx_name_affirmation/v1/verified_name?username=xxx
+
+    Supports:
+        HTTP POST: Creates a new VerifiedName.
+        HTTP GET: Returns an existing VerifiedName (by username or requesting user)
+
+    HTTP POST
+    Creates a new VerifiedName.
+    Expected POST data: {
+        "username": "jdoe",
+        "verified_name": "Jonathan Doe"
+        "profile_name": "Jon Doe"
+        "verification_attempt_id": (Optional)
+        "proctored_exam_attempt_id": (Optional)
+        "is_verified": (Optional)
+    }
+
+    HTTP GET
+        ** Scenarios **
+        ?username=jdoe
+        returns an existing verified name object matching the username
     """
     def get(self, request):
         """
@@ -50,3 +71,36 @@ class VerifiedNameView(AuthenticatedAPIView):
 
         serialized_data = VerifiedNameSerializer(verified_name).data
         return Response(serialized_data)
+
+    def post(self, request):
+        """
+        Create verified name
+        """
+        username = request.data.get('username')
+        if username != request.user.username and not request.user.is_staff:
+            return Response(
+                status=status.HTTP_403_FORBIDDEN,
+                data={"detail": "Must be a Staff User to Perform this request."}
+            )
+
+        serializer = VerifiedNameSerializer(data=request.data)
+        if serializer.is_valid():
+            user = get_user_model().objects.get(username=username) if username else request.user
+            try:
+                create_verified_name(
+                    user,
+                    request.data.get('verified_name'),
+                    request.data.get('profile_name'),
+                    verification_attempt_id=request.data.get('verification_attempt_id', None),
+                    proctored_exam_attempt_id=request.data.get('proctored_exam_attempt_id', None),
+                    is_verified=request.data.get('is_verified', False)
+                )
+                response_status = status.HTTP_200_OK
+                data = {}
+            except VerifiedNameMultipleAttemptIds as exc:
+                response_status = status.HTTP_400_BAD_REQUEST
+                data = {"detail": str(exc)}
+        else:
+            response_status = status.HTTP_400_BAD_REQUEST
+            data = serializer.errors
+        return Response(status=response_status, data=data)
